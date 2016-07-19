c79c625da1a6fa6d9adb4115b147452d0ae9812f
Jon Friskics <jonfriskics@gmail.com>
2016-07-19 11:35:28 -0400

    updated settings, created Procfile and requirements so we can deploy to Heroku

--- Directory/Directory/settings.py
+++ Directory/Directory/settings.py
@@ -120,4 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    BASE_DIR + '/www/static',
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
---
+++ Directory/Procfile
@@ -0,0 +1,1 @@
+web: sh -c "cd Directory && gunicorn Directory.wsgi"
